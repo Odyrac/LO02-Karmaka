22309d226528cdcfded365c6bee76b4dec6a14ef
Odyrac <odyrac@gmail.com>
2023-11-13 15:34:30 +0100

    utils/main

---
+++ main/globalpackage/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+    
+        System.out.println("\u001B[31m██╗  ██╗ █████╗ ██████╗ ███╗   ███╗ █████╗ ██╗  ██╗ █████╗ \u001B[0m");
+        System.out.println("\u001B[31m██║ ██╔╝██╔══██╗██╔══██╗████╗ ████║██╔══██╗██║ ██╔╝██╔══██╗\u001B[0m");
+        System.out.println("\u001B[31m█████╔╝ ███████║██████╔╝██╔████╔██║███████║█████╔╝ ███████║\u001B[0m");
+        System.out.println("\u001B[31m██╔═██╗ ██╔══██║██╔══██╗██║╚██╔╝██║██╔══██║██╔═██╗ ██╔══██║\u001B[0m");
+        System.out.println("\u001B[31m██║  ██╗██║  ██║██║  ██║██║ ╚═╝ ██║██║  ██║██║  ██╗██║  ██║\u001B[0m");
+        System.out.println("\u001B[31m╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝\u001B[0m");
+
+        Partie partie = new Partie();
+        partie.debutPartie(null);
+    }
+
+ 
+
+}
